4ec64d0a9cdd0388dbb0e9c990f6d6553655d7c4
tedcharles111 <frikeallarry@gmail.com>
2026-03-09 15:10:49 +0000

    Fix child_process import (remove .js extension)

--- src/services/buildService.js
+++ src/services/buildService.js
@@ -1,4 +1,4 @@
-import { spawn } from 'child_process.js';
+import { spawn } from "child_process";
 import path from 'path.js';
 import fs from 'fs/promises.js';
 import { config } from '../config/index.js';
